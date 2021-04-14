6bee7e41392222c65c5896a3345c29df746d2ef5
unknown <you@example.com>
2021-04-14 13:08:31 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(".srch").click(function(){
+    $("#search-part").slideDown()
+})
+$(".crs").click(function(){
+    $("#search-part").slideUp()
+})
+$(".come").click(function(){
+    $(".go").fadeToggle()
+})
+$(document).ready(function(){
+    $('.venobox').venobox(); 
+});
+                
